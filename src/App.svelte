4cf2927cc9061c89a77cdd524702cd8f1faf5d65
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2022-07-22 22:22:41 +0300

    v0.2

--- src/App.svelte
+++ src/App.svelte
@@ -12,10 +12,10 @@
 <div class="bg-cover bg-center min-h-screen" style="background-image: url({bg})"></div>
  <div class="absolute inset-0">
   <div  data-aos="zoom-in" class="min-h-screen px-3 sm:px-52 items-center justify-center flex flex-col space-y-10  bg-gradient-to-t from-[black]  boyut mb-5 ">
-    <img class="h-32 sm:mt-0 mt-10 sm:h-52" src={logo2} alt="">
-    <h1 class=" text-center  text-white/70 mt-5 text-font text-lg break-words sm:text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita officia ab quae provident natus! Illum, quia! Debitis, atque incidunt accusantium, quo dicta maiores voluptatibus magni repudiandae illo nobis omnis. Voluptatem?</h1>
+    <img class="h-32 sm:mt-0 mt-10 sm:h-40 xl:mt-32 2xl:h-52 mb-4" src={logo2} alt="">
+    <h1 class=" text-center  text-white/70 mt-5 text-font text-lg break-words sm:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita officia ab quae provident natus! Illum, quia! Debitis, atque incidunt accusantium, quo dicta maiores voluptatibus magni repudiandae illo nobis omnis. Voluptatem?</h1>
     <div class="flex sm:flex-row flex-col items-center gap-5">
-      <a href="https://discord.gg/7GvdW2V7" class="mr-1 mb-1 px-5 sm:px-10 py-2 sm:py-3 mt-4 text-font  outline-none focus:outline-none  ease-linear transition-all duration-150 hover:ring-2 hover:ring-[#404eed] text-white bg-[#404eed] items-center flex space-x-5 font-bold rounded-lg text-lg sm:text-2xl"><i class="fa-brands fa-discord text-2xl sm:text-4xl"></i> <h1>Add To Discord</h1></a>
+      <a target="_blank" href="https://discord.gg/7GvdW2V7" class="mr-1 mb-1 px-5 sm:px-10 py-2 sm:py-3  text-font  outline-none focus:outline-none  ease-linear transition-all duration-150 hover:bg-[#3845cf] text-white bg-[#404eed] items-center flex space-x-5 font-bold rounded-lg text-lg sm:text-2xl"><i class="fa-brands fa-discord text-2xl sm:text-4xl"></i> <h1>Add To Discord</h1></a>
     </div>
    </div>
  </div>  
--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -8,23 +8,23 @@
   }
 </style>
 
-<div class="w-full flex py-4 px-3 z-40 fixed  inset-x-0 top-0 items-center justify-center h-auto">
-  <div class="w-auto outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-20 flex px-8 items-center bg-black justify-between space-x-32 rounded-full cursor-pointer backdrop-blur-xl shadow-xl bg-opacity-50">
-      <img src={logo} class="h-10" alt="">
+<div class="w-full flex py-4  z-40 fixed  inset-x-0 top-0 items-center justify-center h-auto">
+  <div class="w-auto pointer-events-none outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-20 flex px-2 items-center bg-black justify-between space-x-32 rounded-full cursor-pointer backdrop-blur-xl shadow-xl bg-opacity-50">
+      <img src={logo} class="h-10 ml-5" alt="">
         <div class="md:block hidden">
-          <div class="flex space-x-5 lg:space-x-7">
+          <div class="flex space-x-5 pointer-events-auto lg:space-x-7">
             <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">Commands</a>
             <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">Store</a>
             <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">About</a>
             <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">FAQ</a>
           </div>
         </div>
-      <div class="md:block hidden">
-        <a href="login" class="bg-[#404eed] text-style hover:ring-4 text-white font-bold text-xl px-10 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+      <div class="md:block pointer-events-auto hidden">
+        <a href="login" class="bg-[#404eed] mr-3 text-style hover:bg-[#3845cf] text-white font-bold text-xl px-10 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150">
           <span class="text-white text-font font-bold ">Login</span>
         </a>
       </div>
-      <div class="block md:hidden">
+      <div class="block pointer-events-auto md:hidden">
         <div class="bg-[#404eed] ml-5 text-style text-white font-bold text-xl px-5 py-2 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
           <i class="fa-duotone text-2xl fa-bars"></i>
         </div>
