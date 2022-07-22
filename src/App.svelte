cae8eb9e762787dbe429283dfab459423e352f19
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2022-07-22 22:05:26 +0300

    tamam amk

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Navbar from "./lib/navbar.svelte"
+  import logo2 from "./assets/peenuxlogo.svg"
+  import bg from "./assets/backgroundimage.svg"
+</script>
+<style>
+  .text-font {
+    font-family: 'Inter', sans-serif;
+  }
+</style>
+<Navbar />
+<div class="bg-cover bg-center min-h-screen" style="background-image: url({bg})"></div>
+ <div class="absolute inset-0">
+  <div  data-aos="zoom-in" class="min-h-screen px-3 sm:px-52 items-center justify-center flex flex-col space-y-10  bg-gradient-to-t from-[black]  boyut mb-5 ">
+    <img class="h-32 sm:mt-0 mt-10 sm:h-52" src={logo2} alt="">
+    <h1 class=" text-center  text-white/70 mt-5 text-font text-lg break-words sm:text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita officia ab quae provident natus! Illum, quia! Debitis, atque incidunt accusantium, quo dicta maiores voluptatibus magni repudiandae illo nobis omnis. Voluptatem?</h1>
+    <div class="flex sm:flex-row flex-col items-center gap-5">
+      <a href="https://discord.gg/7GvdW2V7" class="mr-1 mb-1 px-5 sm:px-10 py-2 sm:py-3 mt-4 text-font  outline-none focus:outline-none  ease-linear transition-all duration-150 hover:ring-2 hover:ring-[#404eed] text-white bg-[#404eed] items-center flex space-x-5 font-bold rounded-lg text-lg sm:text-2xl"><i class="fa-brands fa-discord text-2xl sm:text-4xl"></i> <h1>Add To Discord</h1></a>
+    </div>
+   </div>
+ </div>  
+
+<div class="min-h-screen bg-black"></div>
+
+
+
---
+++ src/lib/navbar.svelte
@@ -0,0 +1,33 @@
+<script>
+  import logo from "../assets/onlylogo.svg"
+</script>
+
+<style>
+  .text-font {
+    font-family: 'Inter', sans-serif;
+  }
+</style>
+
+<div class="w-full flex py-4 px-3 z-40 fixed  inset-x-0 top-0 items-center justify-center h-auto">
+  <div class="w-auto outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-20 flex px-8 items-center bg-black justify-between space-x-32 rounded-full cursor-pointer backdrop-blur-xl shadow-xl bg-opacity-50">
+      <img src={logo} class="h-10" alt="">
+        <div class="md:block hidden">
+          <div class="flex space-x-5 lg:space-x-7">
+            <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">Commands</a>
+            <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">Store</a>
+            <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">About</a>
+            <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">FAQ</a>
+          </div>
+        </div>
+      <div class="md:block hidden">
+        <a href="login" class="bg-[#404eed] text-style hover:ring-4 text-white font-bold text-xl px-10 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+          <span class="text-white text-font font-bold ">Login</span>
+        </a>
+      </div>
+      <div class="block md:hidden">
+        <div class="bg-[#404eed] ml-5 text-style text-white font-bold text-xl px-5 py-2 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+          <i class="fa-duotone text-2xl fa-bars"></i>
+        </div>
+      </div>
+  </div>
+</div> 
