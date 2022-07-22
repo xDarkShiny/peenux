ec3006163aa4382ff4d902fc8945f265cc26b8ed
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2022-07-22 22:38:39 +0300

    fixed v0.3

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -8,8 +8,8 @@
   }
 </style>
 
-<div class="w-full flex py-4  z-40 fixed  inset-x-0 top-0 items-center justify-center h-auto">
-  <div class="w-auto pointer-events-none outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-20 flex px-2 items-center bg-black justify-between space-x-32 rounded-full cursor-pointer backdrop-blur-xl shadow-xl bg-opacity-50">
+<div class="w-full flex py-4 z-40 fixed  inset-x-0 top-0 items-center justify-center h-auto">
+  <div class="w-auto pointer-events-none outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-20 flex px-2 items-center bg-black justify-between space-x-32 md:space-x-28 lg:space-x-32 rounded-full cursor-pointer backdrop-blur-xl shadow-xl bg-opacity-50">
       <img src={logo} class="h-10 ml-5" alt="">
         <div class="md:block hidden">
           <div class="flex space-x-5 pointer-events-auto lg:space-x-7">
@@ -20,7 +20,7 @@
           </div>
         </div>
       <div class="md:block pointer-events-auto hidden">
-        <a href="login" class="bg-[#404eed] mr-3 text-style hover:bg-[#3845cf] text-white font-bold text-xl px-10 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150">
+        <a href="login" class="bg-[#404eed] mr-3  text-style hover:bg-[#3845cf] text-white font-bold text-xl px-10 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150">
           <span class="text-white text-font font-bold ">Login</span>
         </a>
       </div>
