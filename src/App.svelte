c3ef07821d9267c2f67423be307240d54b69c8e7
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2022-07-24 11:30:48 +0300

    bilmem kaç

--- src/App.svelte
+++ src/App.svelte
@@ -15,23 +15,23 @@
 <div class="bg-cover bg-center min-h-screen" style="background-image: url({bg})"></div>
  <div class="absolute inset-0">
   <div  data-aos="zoom-in" class="min-h-screen flex justify-center items-center px-3 bg-gradient-to-t from-[#202225]  boyut ">
-      <div class="items-center mt-16 sm:mb-32 flex flex-col space-y-5 ">
-        <img class="h-32 sm:mt-0 mt-10 sm:h-40 xl:mt-32 2xl:h-52" src={logo2} alt="">
-        <h1 class=" text-center  text-white/70 mt-5 text-font sm:w-[40rem] text-lg break-words sm:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita officia ab quae provident natus! Illum, quia! Debitis, atque incidunt accusantium, quo dicta maiores voluptatibus magni repudiandae illo nobis omnis. Voluptatem?</h1>
+      <div class="items-center mt-16 sm:mb-32 flex flex-col  ">
+        <img class="h-32 sm:mt-0 mb-5 mt-10 sm:h-40 xl:mt-32 2xl:h-52" src={logo2} alt="">
+        <h1 class=" text-center  text-white/70 mt-3 text-font  sm:w-[40rem]  text-lg break-words ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita officia ab quae provident natus! Illum, quia! Debitis, atque incidunt accusantium, quo dicta maiores voluptatibus magni repudiandae illo nobis omnis. Voluptatem?</h1>
               <div class="relative w-[5rem] sm:w-[10rem] lg:w-[40rem]">
-                <img  src={sarı} class=" absolute right-[11rem] sm:right-[23rem] md:right-[24rem] lg:right-[38rem] top-1.5 h-6" alt="">
+                <img  src={sarı} class=" absolute right-[11rem] sm:right-[23rem] md:right-[24rem] lg:right-[38rem] top-3 h-5" alt="">
               </div>
               <div class="flex sm:flex-row flex-col items-center gap-4">
-                <a target="_blank" href="https://discord.gg/7GvdW2V7" class="mr-1 w-72  mb-4 px-5 sm:px-3 py-2 sm:py-3  text-font justify-center  outline-none focus:outline-none  ease-linear transition-all duration-150 hover:bg-[#3845cf] text-white bg-[#404eed] items-center flex space-x-3 font-bold rounded-[6.5px] text-lg sm:text-xl"><img src={plus} class="w-5 h-5" alt=""> <h1>Add To Discord</h1></a>
-                <a target="_blank" href="https://discord.gg/7GvdW2V7" class="mr-1 backdrop-blur-md bg-white mb-4 w-72 px-5 sm:px-3 py-2 sm:py-3 justify-center text-font  outline-none focus:outline-none  ease-linear transition-all duration-150  text-black  items-center flex space-x-3 font-bold rounded-[6.5px] text-lg sm:text-xl"><i class="fa-brands text-black fa-discord text-2xl "></i> <h1>Join Server</h1></a>
+                <a target="_blank" href="https://discord.gg/7GvdW2V7" class="mr-1 w-72  mb-4 px-5 sm:px-3 py-2  text-font justify-center mt-5  outline-none focus:outline-none  ease-linear transition-all duration-150 hover:bg-[#3845cf] text-white bg-[#404eed] items-center flex space-x-3 font-bold rounded-[6.5px] text-lg sm:text-xl"><img src={plus} class="w-5 h-5" alt=""> <h1>Add To Discord</h1></a>
+                <a target="_blank" href="https://discord.gg/7GvdW2V7" class="w-72   bg-white mb-4 px-5 sm:px-3 py-1.5 justify-center text-font mt-5  outline-none focus:outline-none  ease-linear transition-all duration-150  text-black  items-center flex space-x-3 font-bold rounded-[6.5px] text-lg sm:text-xl"><i class="fa-brands text-black fa-discord text-2xl "></i> <h1>Join Server</h1></a>
              
               </div>
               <div class="relative w-[5rem] sm:w-[10rem] lg:w-[40rem] ">
-                <img  src={mavi} class="absolute bottom-3 left-[11rem] md:bottom-2 sm:left-[24rem] md:left-[24rem] lg:left-[39rem]   h-5" alt="">
+                <img  src={mavi} class="absolute bottom-3 left-[11rem] md:bottom-2 sm:left-[24rem] md:left-[24rem] lg:left-[39rem]   h-4" alt="">
               </div>
       </div>   
    </div>
-   <a href="https://submission.bromb.co/peenux" class="fixed bottom-0 right-0  text-white outline-none focus:outline-none text-font px-4 py-3 mb-5 mr-5 ease-linear transition-all duration-150 items-center bg-[#3845cf] rounded-full cursor-pointer backdrop-blur-xl shadow-xl ">
+   <a href="https://submission.bromb.co/peenux" class="fixed bottom-0 right-0  text-white outline-none focus:outline-none text-font px-3 py-2 mb-5 mr-5 ease-linear transition-all duration-150 items-center bg-[#3845cf] rounded-full cursor-pointer backdrop-blur-xl shadow-xl ">
     <i class="fa-solid fa-circle-question text-2xl"></i>
    </a>
  </div>  
--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -8,18 +8,18 @@
 </style>
 
 <div class="w-full sm:mt-0 flex py-4 bg-gradient-to-b from-[#202225] z-40 fixed  inset-x-0 top-0 items-center justify-center h-auto">
-  <div class="w-auto pointer-events-none outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-20 flex px-2 items-center bg-[#18191c] justify-between space-x-32 md:space-x-28 lg:space-x-32 rounded-full cursor-pointer backdrop-blur-xl shadow-xl bg-opacity-50">
-      <img id="image" src={logo} class="h-10 ml-5" alt="">
+  <div class="w-auto pointer-events-none outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-16 flex px-2 items-center bg-[#18191c] justify-between space-x-32 md:space-x-28 lg:space-x-32 rounded-full cursor-pointer backdrop-blur-xl shadow-xl bg-opacity-50">
+      <img id="image" src={logo} class="h-8 ml-5" alt="">
         <div class="md:block hidden">
           <div class="flex space-x-5 pointer-events-auto lg:space-x-7">
-            <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">Commands</a>
-            <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">Store</a>
-            <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">About</a>
-            <a class="text-font text-white font-bold underline underline-offset-4 text-xl" href="commands">FAQ</a>
+            <a class="text-font text-white font-bold underline underline-offset-4 text-lg" href="commands">Commands</a>
+            <a class="text-font text-white font-bold underline underline-offset-4 text-lg" href="commands">Store</a>
+            <a class="text-font text-white font-bold underline underline-offset-4 text-lg" href="commands">About</a>
+            <a class="text-font text-white font-bold underline underline-offset-4 text-lg" href="commands">FAQ</a>
           </div>
         </div>
       <div class="md:block pointer-events-auto hidden">
-        <a href="login" class="bg-[#404eed] mr-3  text-style hover:bg-[#3845cf] text-white font-bold text-xl px-10 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150">
+        <a href="login" class="bg-[#404eed] mr-3 text-style hover:bg-[#3845cf] text-white font-bold text-md px-7 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150">
           <span class="text-white text-font font-bold ">Login</span>
         </a>
       </div>
